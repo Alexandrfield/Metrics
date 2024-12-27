b7d7dcb66fdef49de608df110460b420d033f717
Alex Polianski <a.palianski@lumos.mobi>
2024-12-27 13:06:36 +0300

    test

--- internal/agent/metrics.go
+++ internal/agent/metrics.go
@@ -95,8 +95,7 @@ func reportMetric(client *http.Client, serverAdderess string, metric common.Metr
 	resp, err := client.Do(req)
 	if err != nil {
 		logger.Debugf("http.NewRequest.Do err: %s\n", err)
-		//logger.Warnf("http.NewRequest.Do err: %s\n", err)
-		//return 0, fmt.Errorf("http.NewRequest.Do err:%w", err)
+		return 0, fmt.Errorf("http.NewRequest.Do err:%w", err)
 	}
 	status := resp.StatusCode
 	defer func() {
