d0cbca26aeea7254bb9911f7cd1dc45a464633cb
Alex Polianski <a.palianski@lumos.mobi>
2025-01-14 15:52:34 +0300

    test

--- internal/requestHandler/request.go
+++ internal/requestHandler/request.go
@@ -79,7 +79,9 @@ func (rep *MetricServer) updateValue(metric *common.Metrics) error {
 	case "gauge":
 		err = rep.memStorage.SetGaugeValue(metric.ID, common.TypeGauge(*metric.Value))
 	case "counter":
-		err = rep.memStorage.SetCounterValue(metric.ID, common.TypeCounter(*metric.Delta))
+		temp := *metric.Delta
+		rep.logger.Debugf("setValue >> counter name: %s;  delta:%d;%d;", metric.ID, temp, common.TypeCounter(temp))
+		err = rep.memStorage.SetCounterValue(metric.ID, common.TypeCounter(temp))
 	default:
 		return fmt.Errorf("unknown type:%s;", metric.MType)
 	}
